58a7a7f16c4901b05a42bf41b377f9ba22a366da
Aman kumar <aman1090.cse16@chitkara.edu.in>
2018-08-29 09:46:37 +0530

    added

---
+++ assigment-6.py
@@ -0,0 +1,13 @@
+#Q1
+dic={1:'abc',2:'lmn',3:'pqr',4:'xyz'}
+list1=[]
+for i in dic.keys():
+    list1.append(i)
+print(list1)
+
+#Q2
+dic={'aman':{'maths':'80','sci':'85'},
+     'ammy':{'maths':'81','sci':'90'}}
+print(dic['aman']['maths'],dic['aman']['sci'])
+print(dic['ammy']['maths'],dic['ammy']['sci'])
+     
